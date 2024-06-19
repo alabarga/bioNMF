80683243120257d83cef696acd54ae9eb9b9d292
Alberto Labarga <alberto.labarga@bsc.es>
2024-06-19 08:05:15 +0200

    Add models

---
+++ models/netmf.py
@@ -0,0 +1,110 @@
+import scipy.io
+import scipy.sparse as sparse
+from scipy.sparse import csgraph
+import numpy as np
+import argparse
+import logging
+import theano
+from theano import tensor as T
+
+logger = logging.getLogger(__name__)
+theano.config.exception_verbosity='high'
+
+
+def load_adjacency_matrix(file, variable_name="network"):
+    data = scipy.io.loadmat(file)
+    logger.info("loading mat file %s", file)
+    return data[variable_name]
+
+def load_label(file, variable_name="group"):
+    data = scipy.io.loadmat(file)
+    logger.info("loading mat file %s", file)
+    label = data[variable_name].todense().astype(np.int64)
+    label = np.array(label)
+    print(label.shape, type(label), label.min(), label.max())
+    return label
+
+def deepwalk_filter(evals, window):
+    for i in range(len(evals)):
+        x = evals[i]
+        evals[i] = 1. if x >= 1 else x*(1-x**window) / (1-x) / window
+    evals = np.maximum(evals, 0)
+    logger.info("After filtering, max eigenvalue=%f, min eigenvalue=%f",
+            np.max(evals), np.min(evals))
+    return evals
+
+def approximate_normalized_graph_laplacian(A, rank, which="LA"):
+    n = A.shape[0]
+    L, d_rt = csgraph.laplacian(A, normed=True, return_diag=True)
+    # X = D^{-1/2} W D^{-1/2}
+    X = sparse.identity(n) - L
+    logger.info("Eigen decomposition...")
+    #evals, evecs = sparse.linalg.eigsh(X, rank,
+    #        which=which, tol=1e-3, maxiter=300)
+    evals, evecs = sparse.linalg.eigsh(X, rank, which=which)
+    logger.info("Maximum eigenvalue %f, minimum eigenvalue %f", np.max(evals), np.min(evals))
+    logger.info("Computing D^{-1/2}U..")
+    D_rt_inv = sparse.diags(d_rt ** -1)
+    D_rt_invU = D_rt_inv.dot(evecs)
+    return evals, D_rt_invU
+
+def approximate_deepwalk_matrix(evals, D_rt_invU, window, vol, b):
+    evals = deepwalk_filter(evals, window=window)
+    X = sparse.diags(np.sqrt(evals)).dot(D_rt_invU.T).T
+    m = T.matrix()
+    mmT = T.dot(m, m.T) * (vol/b)
+    f = theano.function([m], T.log(T.maximum(mmT, 1)))
+    Y = f(X.astype(theano.config.floatX))
+    logger.info("Computed DeepWalk matrix with %d non-zero elements",
+            np.count_nonzero(Y))
+    return sparse.csr_matrix(Y)
+
+def svd_deepwalk_matrix(X, dim):
+    u, s, v = sparse.linalg.svds(X, dim, return_singular_vectors="u")
+    # return U \Sigma^{1/2}
+    return sparse.diags(np.sqrt(s)).dot(u.T).T
+
+def direct_compute_deepwalk_matrix(A, window, b):
+    n = A.shape[0]
+    vol = float(A.sum())
+    L, d_rt = csgraph.laplacian(A, normed=True, return_diag=True)
+    # X = D^{-1/2} A D^{-1/2}
+    X = sparse.identity(n) - L
+    S = np.zeros_like(X)
+    X_power = sparse.identity(n)
+    for i in range(window):
+        logger.info("Compute matrix %d-th power", i+1)
+        X_power = X_power.dot(X)
+        S += X_power
+    S *= vol / window / b
+    D_rt_inv = sparse.diags(d_rt ** -1)
+    M = D_rt_inv.dot(D_rt_inv.dot(S).T)
+    m = T.matrix()
+    f = theano.function([m], T.log(T.maximum(m, 1)))
+    Y = f(M.todense().astype(theano.config.floatX))
+    return sparse.csr_matrix(Y)
+
+def netmf(A, rank=256, dim=128, window=10, negative=1.0, large=True):
+
+    vol = float(A.sum())
+
+    if large:
+        # perform eigen-decomposition of D^{-1/2} A D^{-1/2}
+        # keep top #rank eigenpairs
+        evals, D_rt_invU = approximate_normalized_graph_laplacian(A, rank=rank, which="LA")
+
+        # approximate deepwalk matrix
+        deepwalk_matrix = approximate_deepwalk_matrix(evals, D_rt_invU,
+                window=window,
+                vol=vol, b=negative)
+    else:
+        # directly compute deepwalk matrix
+        deepwalk_matrix = direct_compute_deepwalk_matrix(A,
+                window=window, b=negative)
+        
+    # factorize deepwalk matrix with SVD
+    deepwalk_embedding = svd_deepwalk_matrix(deepwalk_matrix, dim=dim)
+
+    return deepwalk_embedding
+
+
